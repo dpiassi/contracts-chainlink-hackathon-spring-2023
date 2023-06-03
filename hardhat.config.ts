1686c21ddcb2111f52033a3e1309858e1ea16004
Daniel Piassi <daniel.piassi@outlook.com>
2023-06-03 17:56:20 -0300

    :lock: Refreshed and moved Infura API key to env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import { HardhatUserConfig } from "hardhat/config";
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = "71e3c46743934adaa7512cfe2d25c5ec";
+const INFURA_API_KEY = process.env.INFURA_API_KEY!;
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -13,8 +13,6 @@ const INFURA_API_KEY = "71e3c46743934adaa7512cfe2d25c5ec";
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY!;
-
-console.log("SEPOLIA_PRIVATE_KEY", SEPOLIA_PRIVATE_KEY)
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,7 +4,7 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const lockedAmount = ethers.utils.parseEther("0.000001");
 
   const Lock = await ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
